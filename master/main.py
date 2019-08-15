d221d8c11fff930712c23a4880dc9d74e22ed111
huang825172 <1048035187@qq.com>
2019-08-15 09:53:21 +0800

    clear master

--- master/main.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-from server.main import server
-
-if __name__ == "__main__":
-    whnd = ctypes.windll.kernel32.GetConsoleWindow()
-    if whnd != 0:    ctypes.windll.user32.ShowWindow(whnd, 0)
-    ctypes.windll.kernel32.CloseHandle(whnd)
-    Sv = server(6000)
