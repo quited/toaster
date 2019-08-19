268d1290a2c4e573b092dfe04f41d036041e360d
huang825172 <1048035187@qq.com>
2019-08-20 01:13:39 +0800

    finish to removing

--- src/components/mainPageT.vue
+++ src/components/mainPageT.vue
@@ -2,7 +2,7 @@
     <div class="container" style="font-family: StandardBlack">
         <div class="row" style="margin-top:10px">
             <div class="col-12">
-                <h1>这里应该有主界面才对</h1>
+                <h1>这里应该有主界面才对,一会就来写Tabs</h1>
             </div>
         </div>
     </div>
